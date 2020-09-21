5e7a83d0a2f160ab987c4b231d1d9e149269f05f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-21 13:34:58 -0400

    Add the starting version of the iterate.py with the TODO markers.

---
+++ iterator/iterator/iterate.py
@@ -0,0 +1,18 @@
+"""Calculate the powers of two, with iteration, from minimum to maximum values."""
+
+
+def calculate_powers_of_two_for_loop(minimum: int, maximum: int):
+    """Calculate and return the powers of 2 from an inclusive minimum to an exclusive maximum."""
+    powers_list = []
+    for i in range(minimum, maximum):
+        powers_list.append(2 ** i)
+    return powers_list
+
+
+def calculate_powers_of_two_while_loop(minimum: int, maximum: int):
+    """Calculate and return the powers of 2 from an inclusive minimum to an exclusive maximum."""
+    powers_list = []
+    # TODO: Leveraging your notes, the course slides, and the other function,
+    # please provide a complete implementation of this function that passes
+    # the test suite and produces the correct output
+    return powers_list
